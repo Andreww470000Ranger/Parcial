5e2719f244b7f9d97171ecc996794e0acea2c1c6
Andreww470000Ranger <44888081+Andreww470000Ranger@users.noreply.github.com>
2019-03-19 19:26:26 -0600

    Add files via upload

---
+++ Parcialon/Parcial/ClassLibrary1/Class1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        int Num1, Num2;
+
+        //Ejercicio 1
+        public void Multiplicacion() 
+        {
+            Console.Clear();
+            Console.WriteLine("Ingrese el primer numero");
+            Num1 = int.Parse(Console.ReadLine());
+
+            if (Num1 == 0)
+            {
+                Console.WriteLine("Ingrese un valor distinto a 0");
+            }
+            else
+            {
+                Console.WriteLine("Ingrese el segundo numero");
+                Num2 = int.Parse(Console.ReadLine());
+
+                int Producto = Num1 * Num2;
+
+                Console.WriteLine("El producto es: "+Producto);
+
+            }
+
+        }
+
+        //Ejercicio 2
+        public void Division()
+        {
+            Console.Clear();
+            Console.WriteLine("Ingrese el numero que quiere dividir");
+            Num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el numero divisor");
+            Num2 = int.Parse(Console.ReadLine());
+
+            if (Num2 == 0)
+            {
+                Console.WriteLine("ERROR: No se puede dividir entre 0");
+            }
+            else
+            {
+                int Resultado = Num1 / Num2;
+                Console.WriteLine("El Resultado de la division es: " + Resultado);
+            }
+        }
+
+        //Ejercicio 3
+
+        public void Nombre()
+        {
+            Console.Clear();
+            int i = 1;
+            for (i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("INGRESE UN NOMBRE");
+                String Nombre = Console.ReadLine();
+                Console.WriteLine("El nombre ingresado es: "+Nombre);
+            }
+        }
+
+        //Ejercicio 4
+
+        public void Edades()
+        {
+            Console.Clear();
+            int i = 1;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("INGRESE UNA EDAD");
+                int Edad = int.Parse(Console.ReadLine());
+                Console.WriteLine("La edad ingresado es: " + Edad);
+                i++;
+            }while(i <= 5);
+        }
+
+    }
+}
---
+++ Parcialon/Parcial/Ejecuti/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace Ejecuti
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Class1 obj1 = new Class1();
+            Console.WriteLine("¿Que ejercicio desea examinar?");
+            int Opcion = int.Parse(Console.ReadLine());
+
+            switch (Opcion) 
+            {
+                case 1:
+                    obj1.Multiplicacion();
+                    break;
+
+                case 2:
+                    obj1.Division();
+                    break;
+
+                case 3:
+                    obj1.Nombre();
+                    break;
+
+                case 4:
+                    obj1.Edades();
+                    break;
+
+                default:
+                    Console.WriteLine("Opcion incorrecta!!!!");
+                    break;
+
+            }
+            
+        }
+    }
+}
